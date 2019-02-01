7744f6be25f98ee5634a22547b3588fe9a5c2336
mlamin1 <mlamin1@LAPTOP-RQV4CHGC.localdomain>
2019-02-01 11:21:51 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello world..." << endl;
+
+	int num;
+
+	cout << "input a number: ";
+	cin >> num;
+
+
+	cout << (num * num) << endl;
+}
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int hm=10.5;
+	int b=65;
+}
